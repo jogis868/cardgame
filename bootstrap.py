74acb60dddec1b1e613dfc59eb78c04bb1b6ae40
jogis868 <guosteguostukas@gmail.com>
2025-05-22 18:49:36 +0300

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -15,9 +15,15 @@ if not os.path.isdir(VENV_DIR):
     run([PYTHON, "-m", "venv", VENV_DIR])
 
 # 2) install/update pip and project requirements
-PIP = os.path.join(VENV_DIR, "bin", "pip")
+if os.name == "nt":
+    PIP = os.path.join(VENV_DIR, "Scripts", "pip.exe")
+    PY = os.path.join(VENV_DIR, "Scripts", "python.exe")
+else:
+    PIP = os.path.join(VENV_DIR, "bin", "pip")
+    PY = os.path.join(VENV_DIR, "bin", "python")
 run([PIP, "install", "--upgrade", "pip"])
 run([PIP, "install", "-r", "requirements.txt"])
+run([PY, "manage.py", "runserver"])
 
 # 3) run Django
 PY = os.path.join(VENV_DIR, "bin", "python")
